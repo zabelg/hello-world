6caa6890f8540f01a832ced29c616b39a9a45f70
graham <zabelg@gmail.com>
2018-04-10 10:24:33 +0100

    start in deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #curl -X POST localhost:8080/actuator/shutdown
-./jenkins/scripts/stop.sh
-./jenkins/scripts/start.sh
+#./jenkins/scripts/stop.sh
+#./jenkins/scripts/start.sh
+
+java -jar ./demo/target/demo-0.0.1-SNAPSHOT.jar &
 
