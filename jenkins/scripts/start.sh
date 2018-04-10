b59e2c5cf1411ccbb1659855ce3af6b5144320e0
graham <zabelg@gmail.com>
2018-04-10 10:37:35 +0100

    more fixes to start.sh

--- jenkins/scripts/start.sh
+++ jenkins/scripts/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-rm pid.file
-
 echo '### Starting Demo Application'
-set -x
-java -jar ./demo/target/demo-0.0.1-SNAPSHOT.jar & echo $! > ./pid.file &
-set +x
+nohup java -jar ./demo/target/demo-0.0.1-SNAPSHOT.jar > ./log.txt 2>&1 &
+echo $! > ./pid.file
