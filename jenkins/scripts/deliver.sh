c8a998362c692aa6f4ced8c49051f368fb7bfed6
graham <zabelg@gmail.com>
2018-04-10 10:40:17 +0100

    more fixes to start.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 #curl -X POST localhost:8080/actuator/shutdown
-#./jenkins/scripts/stop.sh
-#./jenkins/scripts/start.sh
-
-java -jar ./demo/target/demo-0.0.1-SNAPSHOT.jar &
+./jenkins/scripts/stop.sh
+./jenkins/scripts/start.sh
 
